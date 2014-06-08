a85bd046c5ae1ea3c7b29c5fb29d863283bcb278
Vytautas Astrauskas <vastrauskas@gmail.com>
2014-06-08 23:16:10 +0300

    Pridėtas pakeitimų žurnalas.

---
+++ config/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+{
+    # main.tex options
+    'extra_packages': [
+        ('longtable', ''),
+        ],
+    # style.sty options
+    # other options
+    'git': [
+        ('dump_log', {
+            'output': 'git_log.tex',
+            'format': 'format:%H&&\\\\\n%an & %ai & %s \\\\\n\\hline\n',
+            'path': '../content',
+            }),
+        ],
+    }
