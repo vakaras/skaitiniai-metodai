132962b033f4371e7b3bcb8cc8effa409884f0d8
Vytautas Astrauskas <vastrauskas@gmail.com>
2014-06-09 13:52:05 +0300

    Pridėta slide komanda.

--- config/config.py
+++ config/config.py
@@ -3,6 +3,7 @@
     # main.tex options
     'extra_packages': [
         ('longtable', ''),
+        ('custom', ''),
         ],
     # style.sty options
     'bibliography_resources': [
@@ -15,5 +16,21 @@
             'format': 'format:%H&&\\\\\n%an & %ai & %s \\\\\n\\hline\n',
             'path': '../content',
             }),
+        ],
+    'template_files': [
+        # original
+        'main.tex',
+        'pre-text.tex',
+        'post-text.tex',
+        'chapters.tex',
+        'documentation.cls',
+        'lithuanian.lbx',
+        'title.sty',
+        'global-config.tex',
+        'bibliography.bib',
+        'pglossary.py',
+        'pglossary.sty',
+        # new
+        'custom.sty',
         ],
     }
